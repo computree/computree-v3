c16da9848abdf985a9b0e98c3cba266e4070b330
Alexandre Piboule <alexandre.piboule@onf.fr>
2013-05-18 18:51:20 +0000

    AP : nouvelle appli : générateur d'étape
    git-svn-id: http://rdinnovation.onf.fr/svn/computree@12 0d0f61bb-bbfd-4940-b5ed-fa75979e1372

--- plugintutorialfr/step/tufr_templatestep.cpp
+++ plugintutorialfr/step/tufr_templatestep.cpp
@@ -1,73 +1,68 @@
 #include "tufr_templatestep.h"
 
-// Inclusion des modèles de résultats in et out
+// Inclusion of in and out results models
+#include "ct_result/model/inModel/ct_inresultmodelnotneedinputresult.h"
 #include "ct_result/model/inModel/ct_inresultmodelgroup.h"
 #include "ct_result/model/outModel/ct_outresultmodelgroup.h"
+#include "ct_result/model/inModel/ct_inresultmodelgrouptocopy.h"
+#include "ct_result/model/outModel/ct_outresultmodelgroupcopy.h"
 
-// Inclusion des modèles de groupes in et out
+// Inclusion of in and out groups models
 #include "ct_itemdrawable/model/inModel/ct_inoneormoregroupmodel.h"
 #include "ct_itemdrawable/model/inModel/ct_inzeroormoregroupmodel.h"
 #include "ct_itemdrawable/model/inModel/ct_instandardgroupmodel.h"
 #include "ct_itemdrawable/model/outModel/ct_outstandardgroupmodel.h"
 
-// Inclusion des modèles d'items in et out
+// Inclusion of in and out items models
 #include "ct_itemdrawable/model/inModel/ct_instandarditemdrawablemodel.h"
 #include "ct_itemdrawable/model/outModel/ct_outstandarditemdrawablemodel.h"
 
-// Inclusion de la classe de résultat standard
+// Inclusion of standard result class
 #include "ct_result/ct_resultgroup.h"
 
-#include "qdebug.h" // pour afficher des messages à la console
-#include "math.h"
-
-// Alias des chaines de caractères pour l'indexation des modèles in et out
+// Alias for indexing in and out models
 #define DEF_SearchInResult  "r"
 #define DEF_SearchOutResult "r"
 
-// Constructeur : initialisation des paramètres
+// Constructor : initialization of parameters
 TUFR_TemplateStep::TUFR_TemplateStep(CT_StepInitializeData &dataInit) : CT_AbstractStep(dataInit)
 {
     _parametre = 0;
 }
 
-// Description de l'étape (tooltip du menu contextuel)
+// Step description (tooltip of contextual menu)
 QString TUFR_TemplateStep::getStepDescription() const
 {
     return "Description";
 }
 
-// Méthode de recopie de l'étape
+// Step copy method
 CT_VirtualAbstractStep* TUFR_TemplateStep::createNewInstance(CT_StepInitializeData &dataInit)
 {
-    // cree une copie de cette étape
     return new TUFR_TemplateStep(dataInit);
 }
 
-//////////////////// PROTECTED //////////////////
+//////////////////// PROTECTED METHODS //////////////////
 
-// Création et affiliation des modèles IN
+// Creation and affiliation of IN models
 void TUFR_TemplateStep::createInResultModelListProtected()
 {
 }
 
-// Création et affiliation des modèles OUT
+// Creation and affiliation of OUT models
 void TUFR_TemplateStep::createOutResultModelListProtected()
 {
-
 }
 
-// Création semi-automatique de la boite de dialogue de paramétrage de l'étape
+// Semi-automatic creation of step parameters DialogBox
 void TUFR_TemplateStep::createPostConfigurationDialog()
 {
     CT_StepConfigurableDialog *configDialog = newStandardPostConfigurationDialog();
 
-    configDialog->addDouble("Valeur du paramètre", "unité", 0, 1000, 2, _parametre);
+    configDialog->addDouble("Parameter value", "unit", 0, 1000, 2, _parametre);
 }
 
 void TUFR_TemplateStep::compute()
 {
-
-
-    // PENSER A AJOUTER CETTE ETAPE AU STEPPLUGINMANAGER !!!!!
-
+    // DONT'T FORGET TO ADD THIS STEP TO THE STEPPLUGINMANAGER !!!!!
 }
---
+++ stepcreator/stepcreator.pro
@@ -0,0 +1,12 @@
+QT       += core gui
+
+TARGET = stepcreator
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
