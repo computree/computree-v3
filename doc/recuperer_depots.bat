72b42ff2a787c5555f05c6e97cbbbfae3e44a881
Alexandre Piboule <alexandre.piboule@onf.fr>
2013-09-27 08:54:20 +0000

    git-svn-id: http://rdinnovation.onf.fr/svn/computree@56 0d0f61bb-bbfd-4940-b5ed-fa75979e1372

---
+++ doc/recuperer_depots.bat
@@ -0,0 +1,14 @@
+md exportershared
+md pluginsharedv2
+md computree
+md pluginonfensam
+md pluginonflsis
+md ressources
+
+svn checkout https://computree.unfuddle.com/svn/computree_exportershared/ exportershared
+svn checkout https://computree.unfuddle.com/svn/computree_pluginsharedv2/ pluginsharedv2
+svn checkout https://computree.unfuddle.com/svn/computree_computreev2/ computree
+svn checkout https://computree.unfuddle.com/svn/computree_pluginonfensamv2/ pluginonfensam
+svn checkout https://computree.unfuddle.com/svn/computree_pluginonflsisv2/ pluginonflsis
+svn checkout https://computree.unfuddle.com/svn/computree_ressources/ ressources
+
