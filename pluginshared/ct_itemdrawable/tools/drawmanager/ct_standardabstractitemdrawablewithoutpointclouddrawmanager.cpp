ac3261d14046e36233aed3828030c9fb7729ab19
Alexandre Piboule <alexandre.piboule@onf.fr>
2014-12-31 14:25:02 +0000

    ct_standardabstractitemdrawablewithoutpointclouddrawmanager : Ajout du dessin de la boite englobante
    git-svn-id: http://rdinnovation.onf.fr/svn/computree@247 0d0f61bb-bbfd-4940-b5ed-fa75979e1372

--- pluginshared/ct_itemdrawable/tools/drawmanager/ct_standardabstractitemdrawablewithoutpointclouddrawmanager.cpp
+++ pluginshared/ct_itemdrawable/tools/drawmanager/ct_standardabstractitemdrawablewithoutpointclouddrawmanager.cpp
@@ -2,8 +2,9 @@
 
 #include "ct_abstractitemdrawablewithoutpointcloud.h"
 
-const QString CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::INDEX_CONFIG_CENTER_VISIBLE = CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigCenterVisible();
-const QString CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::INDEX_CONFIG_CENTER_POINT_SIZE = CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigCenterPointSize();
+const QString CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::INDEX_CONFIG_BOUNDING_SHAPE_VISIBLE = CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigBoundingShapeVisible();
+const QString CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::INDEX_CONFIG_BOUNDING_SHAPE_POINT_SIZE = CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigBoundingShapePointSize();
+const QString CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::INDEX_CONFIG_BOUNDING_SHAPE_CENTER_POINT_VISIBLE = CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigBoundingShapeCenterPointVisible();
 
 CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager(QString drawConfigurationName) : CT_AbstractItemDrawableDrawManager(drawConfigurationName.isEmpty() ? "CT_AbstractItemDrawableWithoutPointCloud" : drawConfigurationName)
 {
@@ -19,9 +20,47 @@ void CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::draw(GraphicsV
 
     const CT_AbstractItemDrawableWithoutPointCloud &item = dynamic_cast<const CT_AbstractItemDrawableWithoutPointCloud&>(itemDrawable);
 
-    if(getDrawConfiguration()->getVariableValue(INDEX_CONFIG_CENTER_VISIBLE).toBool())
+    if(getDrawConfiguration()->getVariableValue(INDEX_CONFIG_BOUNDING_SHAPE_VISIBLE).toBool())
     {
-        painter.setPointSize(getDrawConfiguration()->getVariableValue(INDEX_CONFIG_CENTER_POINT_SIZE).toDouble());
+        painter.setPointSize(getDrawConfiguration()->getVariableValue(INDEX_CONFIG_BOUNDING_SHAPE_POINT_SIZE).toDouble());
+        painter.drawPoint(item.minX(), item.minY(), item.minZ());
+        painter.drawPoint(item.maxX(), item.minY(), item.minZ());
+        painter.drawPoint(item.minX(), item.maxY(), item.minZ());
+        painter.drawPoint(item.minX(), item.minY(), item.maxZ());
+        painter.drawPoint(item.maxX(), item.maxY(), item.minZ());
+        painter.drawPoint(item.minX(), item.maxY(), item.maxZ());
+        painter.drawPoint(item.maxX(), item.minY(), item.maxZ());
+        painter.drawPoint(item.maxX(), item.maxY(), item.maxZ());
+        painter.restoreDefaultPointSize();
+
+        // B
+        painter.drawLine(item.minX(), item.minY(), item.minZ(), item.maxX(), item.minY(), item.minZ());
+        painter.drawLine(item.maxX(), item.minY(), item.minZ(), item.maxX(), item.maxY(), item.minZ());
+        painter.drawLine(item.maxX(), item.maxY(), item.minZ(), item.minX(), item.maxY(), item.minZ());
+        painter.drawLine(item.minX(), item.maxY(), item.minZ(), item.minX(), item.minY(), item.minZ());
+
+        // H
+        painter.drawLine(item.minX(), item.minY(), item.maxZ(), item.maxX(), item.minY(), item.maxZ());
+        painter.drawLine(item.maxX(), item.minY(), item.maxZ(), item.maxX(), item.maxY(), item.maxZ());
+        painter.drawLine(item.maxX(), item.maxY(), item.maxZ(), item.minX(), item.maxY(), item.maxZ());
+        painter.drawLine(item.minX(), item.maxY(), item.maxZ(), item.minX(), item.minY(), item.maxZ());
+
+        // G
+        painter.drawLine(item.minX(), item.minY(), item.minZ(), item.minX(), item.minY(), item.maxZ());
+        painter.drawLine(item.minX(), item.minY(), item.maxZ(), item.minX(), item.maxY(), item.maxZ());
+        painter.drawLine(item.minX(), item.maxY(), item.maxZ(), item.minX(), item.maxY(), item.minZ());
+        painter.drawLine(item.minX(), item.maxY(), item.minZ(), item.minX(), item.minY(), item.minZ());
+
+        // D
+        painter.drawLine(item.maxX(), item.minY(), item.minZ(), item.maxX(), item.minY(), item.maxZ());
+        painter.drawLine(item.maxX(), item.minY(), item.maxZ(), item.maxX(), item.maxY(), item.maxZ());
+        painter.drawLine(item.maxX(), item.maxY(), item.maxZ(), item.maxX(), item.maxY(), item.minZ());
+        painter.drawLine(item.maxX(), item.maxY(), item.minZ(), item.maxX(), item.minY(), item.minZ());
+    }
+
+    if(getDrawConfiguration()->getVariableValue(INDEX_CONFIG_BOUNDING_SHAPE_CENTER_POINT_VISIBLE).toBool())
+    {
+        painter.setPointSize(getDrawConfiguration()->getVariableValue(INDEX_CONFIG_BOUNDING_SHAPE_POINT_SIZE).toDouble());
         painter.drawPoint(item.getCenterX(), item.getCenterY(), item.getCenterZ());
         painter.restoreDefaultPointSize();
     }
@@ -32,20 +71,26 @@ CT_ItemDrawableConfiguration CT_StandardAbstractItemDrawableWithoutPointCloudDra
     CT_ItemDrawableConfiguration item = CT_ItemDrawableConfiguration(drawConfigurationName);
 
     item.addAllConfigurationOf(CT_AbstractItemDrawableDrawManager::createDrawConfiguration(drawConfigurationName));
-    item.addNewConfiguration(CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigCenterVisible(), "Centre", CT_ItemDrawableConfiguration::Bool, false);
-    item.addNewConfiguration(CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigCenterPointSize(), "Taille des points (Centre)", CT_ItemDrawableConfiguration::Double, 10.0);
+    item.addNewConfiguration(CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigBoundingShapeVisible(),"Bounding Shape", CT_ItemDrawableConfiguration::Bool, false);
+    item.addNewConfiguration(CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigBoundingShapeCenterPointVisible(), "Centre de la Bounding Shape", CT_ItemDrawableConfiguration::Bool, false);
+    item.addNewConfiguration(CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigBoundingShapePointSize(), "Taille des points (Bounding Shape)", CT_ItemDrawableConfiguration::Double, 10.0);
 
     return item;
 }
 
 // PROTECTED //
 
-QString CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigCenterVisible()
+QString CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigBoundingShapeVisible()
 {
-    return "WOPC_CV";
+    return "WOPC_BSV";
 }
 
-QString CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigCenterPointSize()
+QString CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigBoundingShapePointSize()
 {
-    return "WOPC_CPS";
+    return "WOPC_BSPS";
+}
+
+QString CT_StandardAbstractItemDrawableWithoutPointCloudDrawManager::staticInitConfigBoundingShapeCenterPointVisible()
+{
+    return "WOPC_BSCPS";
 }
